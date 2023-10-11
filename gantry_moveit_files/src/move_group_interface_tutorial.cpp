2cafd24d1cc0575041ddbf89b44596ee64852150
kart1802 <karthikswami.1802@gmail.com>
2023-10-11 19:20:05 +0530

    Picking issue solved, placing issue remaining

--- gantry_moveit_files/src/move_group_interface_tutorial.cpp
+++ gantry_moveit_files/src/move_group_interface_tutorial.cpp
@@ -42,6 +42,7 @@
 #include <std_msgs/Float64MultiArray.h>
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/Quaternion.h>
+#include <shape_msgs/SolidPrimitive.h>
 #include <std_msgs/Bool.h>
 #include <cmath>
 
@@ -89,15 +90,15 @@ void openGripper(trajectory_msgs::JointTrajectory& posture)
 {
   // BEGIN_SUB_TUTORIAL open_gripper
   /* Add both finger joints of panda robot. */
-  posture.joint_names.resize(2);
-  posture.joint_names[0] = "Joint7";
-  posture.joint_names[1] = "Joint8";
+  posture.joint_names.resize(1);
+  posture.joint_names[0] = "Cup_Joint";
+  // posture.joint_names[1] = "Joint8";
 
   /* Set them as open, wide enough for the object to fit. */
   posture.points.resize(1);
-  posture.points[0].positions.resize(2);
-  posture.points[0].positions[0] = 0.0;
-  posture.points[0].positions[1] = 0.0;
+  posture.points[0].positions.resize(1);
+  posture.points[0].positions[0] = 0.25;
+  // posture.points[0].positions[1] = 0.0;
   posture.points[0].time_from_start = ros::Duration(0.5);
   // END_SUB_TUTORIAL
 }
@@ -106,15 +107,15 @@ void closedGripper(trajectory_msgs::JointTrajectory& posture)
 {
   // BEGIN_SUB_TUTORIAL closed_gripper
   /* Add both finger joints of panda robot. */
-  posture.joint_names.resize(2);
-  posture.joint_names[0] = "Joint7";
-  posture.joint_names[1] = "Joint8";
+  posture.joint_names.resize(1);
+  posture.joint_names[0] = "Cup_Joint";
+  // posture.joint_names[1] = "Joint8";
 
   /* Set them as closed. */
   posture.points.resize(1);
-  posture.points[0].positions.resize(2);
-  posture.points[0].positions[0] = 0.04;
-  posture.points[0].positions[1] = -0.04;
+  posture.points[0].positions.resize(1);
+  posture.points[0].positions[0] = 0.00;
+  // posture.points[0].positions[1] = -0.04;
   posture.points[0].time_from_start = ros::Duration(0.5);
   // END_SUB_TUTORIAL
 }
@@ -144,30 +145,31 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group, geometry_m
   // Setting pre-grasp approach
   // ++++++++++++++++++++++++++
   /* Defined with respect to frame_id */
-  // grasps[0].pre_grasp_approach.direction.header.frame_id = "base_link";
-  // /* Direction is set as positive x axis */
-  // grasps[0].pre_grasp_approach.direction.vector.z = -1.0;
-  // grasps[0].pre_grasp_approach.min_distance = 0.05;
-  // grasps[0].pre_grasp_approach.desired_distance = 0.15;
+  grasps[0].pre_grasp_approach.direction.header.frame_id = "base_link";
+  /* Direction is set as positive x axis */
+  grasps[0].pre_grasp_approach.direction.vector.z = -1.0;
+  grasps[0].pre_grasp_approach.min_distance = 0.05;
+  grasps[0].pre_grasp_approach.desired_distance = 0.15;
 
   // // Setting post-grasp retreat
   // // ++++++++++++++++++++++++++
   // /* Defined with respect to frame_id */
-  // grasps[0].post_grasp_retreat.direction.header.frame_id = "base_link";
-  // /* Direction is set as positive z axis */
-  // grasps[0].post_grasp_retreat.direction.vector.z = 1.0;
-  // grasps[0].post_grasp_retreat.min_distance = 0.0;
-  // grasps[0].post_grasp_retreat.desired_distance = 0.0;
+  grasps[0].post_grasp_retreat.direction.header.frame_id = "base_link";
+  /* Direction is set as positive z axis */
+  grasps[0].post_grasp_retreat.direction.vector.z = 1.0;
+  grasps[0].post_grasp_retreat.min_distance = 0.0;
+  grasps[0].post_grasp_retreat.desired_distance = 0.0;
 
   // Setting posture of eef before grasp
   // +++++++++++++++++++++++++++++++++++
-  // openGripper(grasps[0].pre_grasp_posture);
+  openGripper(grasps[0].pre_grasp_posture);
   // END_SUB_TUTORIAL
 
   // BEGIN_SUB_TUTORIAL pick2
   // Setting posture of eef during grasp
   // +++++++++++++++++++++++++++++++++++
-  // closedGripper(grasps[0].grasp_posture);
+  closedGripper(grasps[0].grasp_posture);
+  // std::cout<<grasps;
   // END_SUB_TUTORIAL
   // ros::Duration(0.5).sleep();
   // BEGIN_SUB_TUTORIAL pick3
@@ -434,7 +436,7 @@ int main(int argc, char** argv)
 
   size[0] = 0.6;
   size[1] = 0.02;
-  size[2] = 0.225;
+  size[2] = 0.25;
   pose[0] = 0.4;
   pose[1] = 0.125;
   pose[2] = 0.69;
@@ -641,22 +643,19 @@ int main(int argc, char** argv)
 
   ros::Subscriber box_sub = node_handle.subscribe("/box/loc", 1000, boxlocCallback);
 
-  geometry_msgs::Pose target_pose1;
-  tf2Scalar roll = -3.14, pitch = 0, yaw = 0;
+  // geometry_msgs::Pose target_pose1;
+  // tf2Scalar roll = 0, pitch = 0, yaw = 0;
 
-  tf2::Quaternion orientation;
-  orientation.setRPY(roll, pitch, yaw);
+  // tf2::Quaternion orientation;
+  // orientation.setRPY(roll, pitch, yaw);
 
-  ROS_INFO_STREAM("PoseStamped ps in " << pose1.header.frame_id << " is: " << pose1.pose.position.x << ", " << pose1.pose.position.y << ", " << pose1.pose.position.z);
-  ROS_INFO_NAMED("tutorial", "Add an object into the world_2");
+  // ROS_INFO_STREAM("PoseStamped ps in " << pose1.header.frame_id << " is: " << pose1.pose.position.x << ", " << pose1.pose.position.y << ", " << pose1.pose.position.z);
+  // ROS_INFO_NAMED("tutorial", "Add an object into the world_2");
   
-  target_pose1.orientation.x = 0.000000;
-  target_pose1.orientation.y = 0.000000;
-  target_pose1.orientation.z = 0.000000;
-  target_pose1.orientation.w = 1.000000;
-  target_pose1.position.x = dim[1]*cos(dim[3])/2;
-  target_pose1.position.y = -0.375;
-  target_pose1.position.z = 0.589 + depth +0.04;
+  // target_pose1.orientation = tf2::toMsg(orientation);
+  // target_pose1.position.x = dim[1]*cos(dim[3])/2;
+  // target_pose1.position.y = -0.375;
+  // target_pose1.position.z = 0.589 + depth/2;
   // move_group_interface.setPoseTarget(target_pose1);
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
   // std::cout<<target_pose1;
@@ -694,8 +693,41 @@ int main(int argc, char** argv)
 
   // char name[][20] = {"Table","Table_1","box"};
   // char Planning_group[] = "base_link";
-  addCollisionObject(planning_scene_interface,pose,orientation_1,size,Planning_group,name[10]);
-  visual_tools.trigger();
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.resize(1);
+
+  // Add the first table where the cube will originally be kept.
+  collision_objects[0].id = name[10];
+  std::vector< std::string > object_id;
+  object_id.resize(1);
+  object_id[0] = collision_objects[0].id;
+  object_id.push_back(collision_objects[0].id);
+  collision_objects[0].header.frame_id = Planning_group;
+
+  /* Define the primitive and its dimensions. */
+  collision_objects[0].primitives.resize(1);
+  collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
+  collision_objects[0].primitives[0].dimensions.resize(3);
+  collision_objects[0].primitives[0].dimensions[0] = size[0];
+  collision_objects[0].primitives[0].dimensions[1] = size[1];
+  collision_objects[0].primitives[0].dimensions[2] = size[2];
+
+  /* Define the pose of the table. */
+  collision_objects[0].primitive_poses.resize(1);
+  collision_objects[0].primitive_poses[0].position.x = pose[0];
+  collision_objects[0].primitive_poses[0].position.y = pose[1];
+  collision_objects[0].primitive_poses[0].position.z = pose[2];
+  collision_objects[0].primitive_poses[0].orientation.w = orientation_1[3];
+  collision_objects[0].primitive_poses[0].orientation.x = orientation_1[0];
+  collision_objects[0].primitive_poses[0].orientation.y = orientation_1[1];
+  collision_objects[0].primitive_poses[0].orientation.z = orientation_1[2];
+  // END_SUB_TUTORIAL
+
+  collision_objects[0].operation = collision_objects[0].ADD;
+
+  planning_scene_interface.applyCollisionObjects(collision_objects);
+  // addCollisionObject(planning_scene_interface,pose,orientation_1,size,Planning_group,name[10]);
+  // visual_tools.trigger();
   bool success = (move_group_interface.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   
 
@@ -720,7 +752,92 @@ int main(int argc, char** argv)
   // geometry_msgs::PoseStamped pose1 = move_group_interface.getCurrentPose();
   
   // ROS_INFO_STREAM("PoseStamped ps in " << pose1.header.frame_id << " is: " << pose1.pose.position.x << ", " << pose1.pose.position.y << ", " << pose1.pose.position.z);
-  pick(move_group_interface,target_pose1);
+  // pick(move_group_interface,target_pose1);
+  // move_group_interface.setPlanningTime(45.0);
+
+  // addCollisionObjects(planning_scene_interface);
+
+  // Wait a bit for ROS things to initialize
+  ros::Duration(1.0).sleep();
+  moveit_msgs::CollisionObject object_to_attach;
+  object_to_attach.id = "cube1";
+  // object_to_attach.header.frame_id = frame_id;
+  // object_to_attach.id = "cube";
+  shape_msgs::SolidPrimitive primitive;
+
+  // std::vector<moveit_msgs::CollisionObject> collision_objects;
+  geometry_msgs::Pose target_pose1;
+  tf2Scalar roll = 0, pitch = 0, yaw = 0;
+
+  tf2::Quaternion orientation;
+  orientation.setRPY(roll, pitch, yaw);
+
+  ROS_INFO_STREAM("PoseStamped ps in " << pose1.header.frame_id << " is: " << pose1.pose.position.x << ", " << pose1.pose.position.y << ", " << pose1.pose.position.z);
+  ROS_INFO_NAMED("tutorial", "Add an object into the world_2");
+  
+  target_pose1.orientation = tf2::toMsg(orientation);
+  // target_pose1.position.x = dim[1]*cos(dim[3])/2;
+  // target_pose1.position.y = -0.375;
+  // target_pose1.position.z = 0.589 + depth/2;
+  target_pose1.position.x = 0;
+  target_pose1.position.y = 0;
+  target_pose1.position.z = depth/2 + 0.035;
+
+  for(int j=0;j<4;j++)
+  {
+    std::cout << dim[j] << "\t";
+  }
+
+  // Define the size of the box in meters
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[primitive.BOX_X] = dim[1];
+  primitive.dimensions[primitive.BOX_Y] = dim[0];
+  primitive.dimensions[primitive.BOX_Z] = depth;
+  object_to_attach.header.frame_id = move_group_interface.getEndEffectorLink();
+  // pick(move_group_interface,target_pose1);
+  object_to_attach.primitives.push_back(primitive);
+  object_to_attach.primitive_poses.push_back(target_pose1);
+  object_to_attach.operation = object_to_attach.ADD;
+  // collision_objects.push_back(object_to_attach);
+  // planning_scene_interface.removeCollisionObjects(object_id);
+  // planning_scene_interface.applyCollisionObject(object_to_attach);
+  // planning_scene_interface.removeCollisionObjects(primitive);
+
+  // std::vector<moveit_msgs::CollisionObject> collision_objects;
+
+  // primitive.dimensions[primitive.BOX_X] = 0.8;
+  // primitive.dimensions[primitive.BOX_Y] = 0.4;
+  // primitive.dimensions[primitive.BOX_Z] = 0.15;
+
+  // // Define a pose for the box (specified relative to frame_id)
+  // box_pose.orientation.w = 1.0;
+  // box_pose.position.x = 0.0;
+  // box_pose.position.y = 0.6;
+  // box_pose.position.z = 0.075;
+
+  // collision_object.primitives.push_back(primitive);
+  // collision_object.primitive_poses.push_back(box_pose);
+  // collision_object.operation = collision_object.ADD;
+
+  // collision_objects.push_back(collision_object);
+
+  // Now, let's add the collision object into the world
+  // (using a vector that could contain additional objects)
+  ROS_INFO_NAMED("tutorial", "Add an object into the world");
+  planning_scene_interface.addCollisionObjects(collision_objects);
+
+  ROS_INFO_NAMED("tutorial", "Attach the object to the robot");
+  move_group_interface.attachObject(collision_objects[0].id, "Z_Rot", {"Cup"});
+
+  visual_tools.publishText(text_pose, "Object attached to robot", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+
+
+  // visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+  ros::Duration(1.0).sleep();
+
+
   // visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
   grasp_state.data = true;
   for(int i = 0;i < 100; i++)
@@ -789,6 +906,8 @@ int main(int argc, char** argv)
 
   move_group_interface.move();
 
+  move_group_interface.detachObject(collision_objects[0].id);
+
   ros::ServiceClient grasp_client_detach = node_handle.serviceClient<gazebo_ros_link_attacher::Attach>("/link_attacher_node/detach");
   gazebo_ros_link_attacher::Attach grasp_detach;
 
@@ -815,6 +934,39 @@ int main(int argc, char** argv)
     ROS_ERROR("NAHI HUA BHAI :(");
     // continue;
   } 
+
+  joint_group_positions[0] = 0.0; //x
+  joint_group_positions[1] = 0.0 ; //y
+  joint_group_positions[2] = 0.0; //z
+  joint_group_positions[3] = box_loc[3]; //end-effector
+  joint_group_positions[4] = 0; //end-effector
+  // std::cout<<"\n";
+  // std::cout<<joint_group_positions;
+  move_group_interface.setJointValueTarget(joint_group_positions);
+  move_group_interface.setMaxVelocityScalingFactor(0.05);
+  move_group_interface.setMaxAccelerationScalingFactor(0.05);
+
+  success = (move_group_interface.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  
+
+  // geometry_msgs::Pose goal = move_group_interface.getPoseTargets();
+  // std::cout<< goal;
+
+  // ROS_INFO_STREAM("PoseStamped ps in " << goal.header.frame_id << " is: " << pose1.pose.position.x << ", " << pose1.pose.position.y << ", " << pose1.pose.position.z);
+  ROS_INFO_NAMED("tutorial", "Visualizing plan 2 (joint space goal) %s", success ? "" : "FAILED");
+
+  // // Visualizing plans
+  // // ^^^^^^^^^^^^^^^^^
+  // // We can also visualize the plan as a line with markers in RViz.
+  // ROS_INFO_NAMED("tutorial", "Visualizing plan 1 as trajectory line");
+
+  visual_tools.deleteAllMarkers();
+  visual_tools.publishText(text_pose, "Joint Space Goal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+  visual_tools.trigger();
+  // visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+
+  move_group_interface.move();  
 
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
 
